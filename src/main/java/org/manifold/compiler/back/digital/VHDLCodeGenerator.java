613f08c5f3e908bcc1cd11a2179a88c693de83e4
Murphy Berzish <murphy.berzish@gmail.com>
2014-07-26 22:46:24 -0400

    code gen fixes for registers

--- src/main/java/org/manifold/compiler/back/digital/VHDLCodeGenerator.java
+++ src/main/java/org/manifold/compiler/back/digital/VHDLCodeGenerator.java
@@ -145,7 +145,7 @@ public class VHDLCodeGenerator {
           outputNets.add(outputNet);
         } else {
           // this is an internal node
-          currentNodes.put(escapeIdentifier(nodeName), node);
+          currentNodes.put(nodeName, node);
         }
       }
     } catch (UndeclaredIdentifierException e) {
@@ -346,7 +346,7 @@ public class VHDLCodeGenerator {
         boolean clockActiveHigh = ((BooleanValue) node
             .getAttribute("clockActiveHigh")).toBoolean();
 
-        String processName = "register_" + nodeName;
+        String processName = escapeIdentifier("register_" + nodeName);
         stmts.append(processName);
         stmts.append(": process (");
         // sensitivity list
@@ -364,7 +364,7 @@ public class VHDLCodeGenerator {
           stmts.append(sigOut).append(" <= ")
               .append(booleanToBit(initialValue)).append(";").append(newline);
           // clocked logic
-          stmts.append("elsif(");
+          stmts.append("elsif ");
           if (clockActiveHigh) {
             stmts.append("rising_edge(");
           } else {
@@ -376,7 +376,7 @@ public class VHDLCodeGenerator {
           stmts.append("end if;").append(newline);
         } else {
           // clocked logic
-          stmts.append("if (");
+          stmts.append("if ");
           if (clockActiveHigh) {
             stmts.append("rising_edge(");
           } else {
@@ -394,6 +394,7 @@ public class VHDLCodeGenerator {
               .append(newline);
           stmts.append("end if;").append(newline);
         }
+        stmts.append("end if;").append(newline);
         stmts.append("end process ").append(processName).append(";")
             .append(newline);
       } catch (UndeclaredIdentifierException | UndeclaredAttributeException e) {
